5290c3b0e3f26b0fe172e139ee1f1278790552d0
Silviu Catrina <silviu.catrina@gmail.com>
2020-02-12 06:39:42 +0200

    Add display question functionality

--- data_manager.py
+++ data_manager.py
@@ -1,9 +1,13 @@
 import csv
 
 
-def get_csv_data(question_id=None):
+DATA_HEADER = ['id', 'submission_time', 'view_number',
+               "vote_number", "title", "message", "image"]
+
+
+def get_all_questions():
     '''
-    param question_id: 
+    param question_id:
         If given it will act as a filter and return the dictionary of a specific Question
         If not give it will return a list of dictionaries with all the given details
     '''
@@ -15,23 +19,28 @@ def get_csv_data(question_id=None):
 
         for row in csv_reader:
             question = dict(row)
-
-            if question_id is not None and question_id == question['id']:
-                return question
             user_questions.append(question)
     return user_questions
 
 
-def get_all_questions(convert_line_breaks=False):
+def get_all_answers():
+    '''
+    param question_id:
+        If given it will act as a filter and return the dictionary of a specific Question
+        If not give it will return a list of dictionaries with all the given details
+    '''
 
-    all_questions = get_csv_data()
-    if convert_line_breaks:
-        for question in all_questions:
-            question['title'] = convert_line_breaks_to_br(question['title'])
-            question['message'] = convert_line_breaks_to_br(
-                question['message'])
+    user_answers = []
 
-    return all_questions
+    with open('sample_data/answer.csv') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+
+        for row in csv_reader:
+            answer = dict(row)
+
+            user_answers.append(answer)
+
+    return user_answers
 
 
 def convert_line_breaks_to_br(original_str):
@@ -39,11 +48,11 @@ def convert_line_breaks_to_br(original_str):
 
 
 def sort_questions():
-    ''' 
+    '''
     Returns a dictionary with the questions sorted by submitted time
     '''
     submission_times = []
-    all_questions = get_all_questions(convert_line_breaks=True)
+    all_questions = get_all_questions()
     for question in all_questions:
         submission_times.append(question['submission_time'])
     sorted_times = sorted(submission_times, reverse=True)
@@ -53,3 +62,41 @@ def sort_questions():
             if question['submission_time'] == time:
                 sorted_questions.append(question)
     return sorted_questions
+
+
+def get_question_by_id(question_id):
+    all_questions = get_all_questions()
+
+    for question in all_questions:
+        if question_id == question['id']:
+            return question
+
+    return None
+
+
+def get_answer_by_id(answer_id):
+    all_answers = get_all_answers()
+
+    for answer in all_answers:
+        if answer_id == answer['id']:
+            return answer
+
+    return None
+
+
+def add_question_to_file(question, append=True):
+
+    existing_data = get_all_questions()
+
+    with open('question.csv', 'a') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=DATA_HEADER)
+        csv_writer.writeheader()
+        for row in existing_data:
+            if not append:
+                if row['id'] == question['id']:
+                    row = question
+
+            csv_writer.writerow()
+
+        if append:
+            csv_writer.writerow(question)
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, redirect, url_for
 
 import data_manager as dmg
+from datetime import datetime
 
 app = Flask('__main__')
 
@@ -12,14 +13,40 @@ def home():
                      "vote number", "title", "message", "image"]
     all_questions = dmg.sort_questions()
 
-    print(all_questions)
-
     return render_template("list.html", all_questions=all_questions, table_heading=TABLE_HEADING)
 
 
-@app.route('/question/<question_id>')
-def question(question_id):
-    pass
+@app.route('/question/<question_id>', methods=['GET', 'POST'])
+def question(question_id: int):
+
+    now = datetime.now()
+
+    if request.method == 'POST':
+        user_question = {
+            'id': question_id,
+            'submission_time': now.strftime("%H:%M:%S"),
+            'view_number': 0,
+            'vote_number': 0,
+            'title': request.form['title'],
+            'message': request.form['message'],
+            'image': ''
+        }
+
+        dmg.add_question_to_file(user_question)
+        return redirect('/')
+
+    user_question = dmg.get_question_by_id(question_id)
+    user_answers = dmg.get_answer_by_id(question_id)
+
+    empty = False
+
+    if user_answers == None:
+        empty = True
+
+    print(user_answers)
+
+    return render_template('question.html', user_question=user_question, user_answers=user_answers,
+                           question_id=question_id, empty=empty)
 
 
 @app.route('/add-question')
